b6874bbf6d87e9a4dfb94e84b666310d00c7a745
Scarsz <truescarsz@gmail.com>
2018-12-31 11:15:55 -0800

    fix token retrieve

--- src/main/java/github/scarsz/discordsupportbot/Main.java
+++ src/main/java/github/scarsz/discordsupportbot/Main.java
@@ -10,7 +10,7 @@ import java.nio.charset.Charset;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String token = FileUtils.readFileToString(new File(".token"), Charset.forName("UTF-8"));
+        String token = FileUtils.readFileToString(new File(".token"), Charset.forName("UTF-8")).replace("\n", "").replaceAll("/[^A-Za-z0-9.]/", "");
         if (StringUtils.isBlank(token)) {
             System.out.println("No bot token provided");
             System.exit(1);
